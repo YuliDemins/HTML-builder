c5f2c2a85cae6ee19d12298b12e1b9d7306d6cc3
Yulia <yudemins@mail.ru>
2022-11-05 23:43:57 +0300

    feat: add 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+
+  fs.stat(path.join(__dirname, 'files-copy'), err => {
+    if (!err) {
+        fs.readdir(path.join(__dirname, 'files-copy'), (_, files) => {
+          files.forEach(file => {
+                fs.unlink(path.join(__dirname, 'files-copy', file), err => {
+                  if (err) throw err;
+                  console.log('file remove')
+          })})
+        })
+      }
+});
+copyFolder()
+
+function copyFolder(){
+  fs.mkdir(path.join(__dirname, 'files-copy'),{ recursive: true }, err => {
+    if (err) throw err;
+    console.log('dir created')
+  });
+
+  fs.readdir(path.join(__dirname, 'files'), (_, files) => {
+    files.forEach(file => {
+      fs.stat(path.resolve(__dirname, 'files', file), ( _, stats) => {
+        if (stats.isFile()) {
+          fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), err => {
+            if (err) throw err;
+            console.log('file copy')});
+      } 
+    })})})
+}
 
